bbad4ab4d1dcd52f43f8c3638522eba9eb11e941
R-vijayendren <vijayendren.r@gmail.com>
2019-03-15 10:52:10 +0530

    Update main2.java

--- main2.java
+++ main2.java
@@ -9,11 +9,18 @@ public class main2 {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int a=sc.nextInt();
+		if(a<0)
+		{
+			System.out.println("invalid number")
+		}
+		else
+		{
 		if((a%2)==0){
 			System.out.println("The num is even...");
 		}
 		else{
 			System.out.println("The num is odd...");
+		}
 		}
 		sc.close();
 		
